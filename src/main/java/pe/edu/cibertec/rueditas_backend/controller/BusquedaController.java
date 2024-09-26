5b8cb2442c8b4d9680f94d38b8a875ea77f297be
NicolasSembrera <nsembrera@gmail.com>
2024-09-25 23:49:22 -0500

    Subiendo proyecto

---
+++ src/main/java/pe/edu/cibertec/rueditas_backend/controller/BusquedaController.java
@@ -0,0 +1,9 @@
+package pe.edu.cibertec.rueditas_backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/autos")
+public class BusquedaController {
+}
